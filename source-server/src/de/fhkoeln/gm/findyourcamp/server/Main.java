bf2102345b6e6bd5a90272252aaca140f12df37a
Dominik Schilling <dominikschilling+git@gmail.com>
2014-01-13 03:59:54 +0100

    Datenbank

--- source-server/src/de/fhkoeln/gm/findyourcamp/server/Main.java
+++ source-server/src/de/fhkoeln/gm/findyourcamp/server/Main.java
@@ -1,6 +1,11 @@
 package de.fhkoeln.gm.findyourcamp.server;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -286,7 +291,84 @@ public class Main {
 	public static void main(String [] args) {
 		Main ccsClient = new Main();
 
+
+
+
 		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	        try {
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/eis", "eis", "eis");
+				Statement statement = con.createStatement();
+				statement.executeUpdate("drop table if exists person");
+			      statement.executeUpdate("create table person (id int, name text)");
+			      statement.executeUpdate("insert into person values(1, 'leo')");
+			      statement.executeUpdate("insert into person values(2, 'yui')");
+			      ResultSet rs = statement.executeQuery("select * from person");
+			      while(rs.next())
+			      {
+			        // read the result set
+			        System.out.println("name = " + rs.getString("name"));
+			        System.out.println("id = " + rs.getInt("id"));
+			      }
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+        /*
+		 try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		    Connection connection = null;
+		    try
+		    {
+		      // create a database connection
+		      connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		      Statement statement = connection.createStatement();
+		      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+		      statement.executeUpdate("drop table if exists person");
+		      statement.executeUpdate("create table person (id integer, name string)");
+		      statement.executeUpdate("insert into person values(1, 'leo')");
+		      statement.executeUpdate("insert into person values(2, 'yui')");
+		      ResultSet rs = statement.executeQuery("select * from person");
+		      while(rs.next())
+		      {
+		        // read the result set
+		        System.out.println("name = " + rs.getString("name"));
+		        System.out.println("id = " + rs.getInt("id"));
+		      }
+		    }
+		    catch(SQLException e)
+		    {
+		      // if the error message is "out of memory",
+		      // it probably means no database file is found
+		      System.err.println(e.getMessage());
+		    }
+		    finally
+		    {
+		      try
+		      {
+		        if(connection != null)
+		          connection.close();
+		      }
+		      catch(SQLException e)
+		      {
+		        // connection close failed.
+		        System.err.println(e);
+		      }
+		    }
+		*/
+		/*try {
 
 			try {
 			  ccsClient.connect(CSS_USERNAME + "@gcm.googleapis.com", CSS_PASSWORD);
@@ -313,7 +395,7 @@ public class Main {
 	} catch ( Exception e ) {
 	} finally {
 	        System.out.println( "Service killed" );
-	}
+	}*/
 
 	}
 
