35a1d12edeebfd139478233a52d31556d1352ebc
Dominik Schilling <dominikschilling+git@gmail.com>
2014-01-17 18:10:55 +0100

    Fix merge conflict

--- source/src/de/fhkoeln/gm/findyourcamp/app/MainActivity.java
+++ source/src/de/fhkoeln/gm/findyourcamp/app/MainActivity.java
@@ -14,21 +14,19 @@ import de.fhkoeln.gm.findyourcamp.app.gcm.MessageConstants;
 import de.fhkoeln.gm.findyourcamp.app.utils.GooglePlayServices;
 import de.fhkoeln.gm.findyourcamp.app.utils.Logger;
 
-<<<<<<< HEAD
+
 /**
- * 
- * 
+ * Startpunkt.
+ *
  *
  */
-public class MainActivity extends Activity implements ActionBar.OnNavigationListener {
-=======
 public class MainActivity extends Activity {
->>>>>>> ca49fa458d1d63db77efce52c895b3c8b094ac0a
+
 
 	private GcmClient client;
 
     /**
-     * 
+     *
      */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
